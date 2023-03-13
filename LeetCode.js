852f86653591d5abaefb4b951a62c0f8d965ab90
DiegoCascavita <sportscenter857@gmail.com>
2023-03-13 12:56:52 -0500

    adding the 75 most common questions by NeetCodeYT reddit

--- LeetCode.js
+++ LeetCode.js
@@ -97,3 +97,19 @@ const fibonacci = (n) =>{
   return fib[n]
 } 
 console.log(fibonacci(9))
+
+
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+let containsDuplicate = function(nums) {
+  nums.sort((a,b)=>{
+      return a - b
+  })
+
+  for(let i = 1; i < nums.length; i ++){
+    if( nums[i] === nums[i-1] ){
+      return true
+    }
+  }
+  return false
+}
+console.log(containsDuplicate([1,2,3,1]))
--- tests.js
+++ tests.js
@@ -1,57 +1,13 @@
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+let containsDuplicate = function(nums) {
+  nums.sort((a,b)=>{
+      return a - b
+  })
+
+  for(let i = 1; i < nums.length; i ++){
+    if( nums[i] === nums[i-1] ){
+      return true
+    }
   }
+  return false
 }
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  append(val) {
-    const newNode = new ListNode(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.size++;
-  }
-
-  remove(val) {
-    let currNode = this.head;
-    let prevNode = null;
-
-    while (currNode !== null) {
-      if (currNode.val === val) {
-        if (prevNode === null) {
-          this.head = currNode.next;
-        } else {
-          prevNode.next = currNode.next;
-        }
-        this.size--;
-        return true;
-      }
-      prevNode = currNode;
-      currNode = currNode.next;
-    }
-    return false;
-  }
-
-  get(index) {
-    if (index < 0 || index >= this.size) {
-      return null;
-    }
-    let currNode = this.head;
-    for (let i = 0; i < index; i++) {
-      currNode = currNode.next;
-    }
-    return currNode.val;
-  }
-} 
+console.log(containsDuplicate([1,2,3,1]))
