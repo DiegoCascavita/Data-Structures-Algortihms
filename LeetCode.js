ea9e76b743b9c60bbd15e0d022531385a3ff389a
DiegoCascavita <sportscenter857@gmail.com>
2023-03-15 07:46:05 -0500

    training hashmap

--- LeetCode.js
+++ LeetCode.js
@@ -113,3 +113,26 @@ let containsDuplicate = function(nums) {
   return false
 }
 console.log(containsDuplicate([1,2,3,1]))
+
+/*
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]*/
+
+function groupAnagrams(strs) {
+  //1. create a hashmap
+  const hashTable = {}
+  //2. iterate over the object
+  for(let str of strs){
+  //3. separate and sort each character
+    const sortedStr = str.split("").sort().join()
+  //4. if the sortedString isn´t in hashtable add it in an array
+    if(!hashTable[sortedStr]){
+      hashTable[sortedStr] = []
+    }//if it is push the str into the array
+    hashTable[sortedStr].push(str)
+  }//return the hashtable
+  return Object.values(hashTable)
+}
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))   
--- tests.js
+++ tests.js
@@ -1,78 +1,22 @@
- //hash map with KEY & VALUES
+/*
+Example 1:
 
- class HashTable {
-  constructor(size=53){
-    this.keyMap = new Array(size);
-  }
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]*/
 
-  _hash(key) {
-    let total = 0;
-    let WEIRD_PRIME = 31;
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-      let char = key[i];
-      let value = char.charCodeAt(0) - 96
-      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
-    }
-    return total;
-  }
-  set(key,value){
-    let index = this._hash(key);
-    if(!this.keyMap[index]){
-      this.keyMap[index] = [];
-    }
-    this.keyMap[index].push([key, value]);
-  }
-  get(key){
-    let index = this._hash(key);
-    if(this.keyMap[index]){
-      for(let i = 0; i < this.keyMap[index].length; i++){
-        if(this.keyMap[index][i][0] === key) {
-          return this.keyMap[index][i][1]
-        }
-      }
-    }
-    return undefined;
-  }
-  keys(){
-    let keysArr = [];
-    for(let i = 0; i < this.keyMap.length; i++){
-      if(this.keyMap[i]){
-        for(let j = 0; j < this.keyMap[i].length; j++){
-          if(!keysArr.includes(this.keyMap[i][j][0])){
-            keysArr.push(this.keyMap[i][j][0])
-          }
-        }
-      }
-    }
-    return keysArr;
-  }
-  values(){
-    let valuesArr = [];
-    for(let i = 0; i < this.keyMap.length; i++){
-      if(this.keyMap[i]){
-        for(let j = 0; j < this.keyMap[i].length; j++){
-          if(!valuesArr.includes(this.keyMap[i][j][1])){
-            valuesArr.push(this.keyMap[i][j][1])
-          }
-        }
-      }
-    }
-    return valuesArr;
-  }
+function groupAnagrams(strs) {
+  //1. create a hashmap
+  const hashTable = {}
+  //2. iterate over the object
+  for(let str of strs){
+  //3. separate and sort each character
+    const sortedStr = str.split("").sort().join()
+  //4. if the sortedString isn´t in hashtable add it in an array
+    if(!hashTable[sortedStr]){
+      hashTable[sortedStr] = []
+    }//if it is push the str into the array
+    hashTable[sortedStr].push(str)
+  }//return the hashtable
+  return Object.values(hashTable)
 }
-
-let ht = new HashTable(17);
-ht.set("maroon","#800000")
-ht.set("yellow","#FFFF00")
-ht.set("olive","#808000")
-ht.set("salmon","#FA8072")
-ht.set("lightcoral","#F08080")
-ht.set("mediumvioletred","#C71585")
-ht.set("plum","#DDA0DD")
-ht.set("purple","#DDA0DD")
-ht.set("violet","#DDA0DD")
-
-
-ht.keys().forEach(function(key){
-  console.log(ht.get(key));
-})
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
