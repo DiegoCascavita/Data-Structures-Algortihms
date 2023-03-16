d8add810ac7246c2a185e3a2b3b6bd4d4969367a
DiegoCascavita <sportscenter857@gmail.com>
2023-03-16 18:30:58 -0500

    improving (n^2)

--- tests.js
+++ tests.js
@@ -1,12 +1,15 @@
-const twoSum = function(numbers, target) {
-  const map = new Map();
-  for (let i = 0; i < numbers.length; i++) {
-    const complement = target - numbers[i];
-    if (map.has(complement)) {
-      return [map.get(complement) + 1, i + 1];
+function twoSum(numbers, target){
+  const map = new Map()
+
+  for(let i = 0; i < numbers.length; i){
+    const complement = target - numbers[i]
+    if(map.has(complement)){
+      return [map.get(complement) + 1, i + 1]
     }
-    map.set(numbers[i], i);
+    map.set(numbers[i], i)
   }
-  return false;
-};
-console.log(twoSum([11,15,2,7,],9))
+}
+
+console.log(twoSum([2,7,11,15,],9))
+console.log(twoSum([2,3,4],6))
+// console.log(twoSum([[-1,0],-1))
