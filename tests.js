92ed0520059234a2a3fec59ffb8504eed48c63ee
DiegoCascavita <sportscenter857@gmail.com>
2023-03-21 09:10:41 -0500

    rotated sorted array

--- tests.js
+++ tests.js
@@ -1,31 +1,26 @@
-// **Binary search in each row - O(mlog n)**
-var searchMatrix1 = function(matrix, target) {
-  for(let i = 0; i < matrix.length; i++) {
-      let start = 0, end = matrix[0].length - 1
+var search = function(nums, target) {
+  let left = 0, right = nums.length - 1; 
 
-      while(start <= end) {
-          let mid = Math.floor((start + end) / 2)
-          if(matrix[i][mid] === target) return true
-          
-          if(matrix[i][mid] > target) end = mid - 1
-          else start = mid + 1
+  
+
+  while (left <= right) {
+      let mid = Math.floor((left + right) / 2); 
+      if (nums[mid] === target) {
+          return mid; 
+      } else if (nums[mid] >= nums[left]) { // left side is sorted 
+          if (target >= nums[left] && target < nums[mid]) { // target is in the left side 
+              right = mid - 1; 
+          } else { // target is in the right side 
+              left = mid + 1; 
+          }
+      } else { // right side is sorted 
+          if (target > nums[mid] && target <= nums[right]) { // target is in the right side  
+              left = mid + 1;  
+          } else { // target is in the left side  
+              right = mid - 1;  
+          }
       }
   }
-  return false
+  return -1;  
 }
-console.log(searchMatrix1([[1,3,5,7],[10,11,16,20],[23,30,34,60]],20))
-
-const searchMatrix = (matrix, target) => {   
-  //1. iterate trough the matrix arrays
-  for(let i = 0; i < matrix.length; i++){
-    let start = 0, end = matrix[0].length - 1
-
-      while(start <= end){
-        let mid = Math.floor((start + end) / 2)
-          if(matrix[i][mid] === target) return true
-          else if(matrix[i][mid] < target) start = mid + 1
-          else end = mid - 1
-      }
-  }
-  return false
-}
+console.log(search([4,5,6,7,0,1,2],0))
