f0e321589c360b2b45bb06d72fe0c849bbb535af
DiegoCascavita <sportscenter857@gmail.com>
2023-03-24 08:16:54 -0500

    contain duplicate

--- tests.js
+++ tests.js
@@ -1,2 +1,11 @@
+function containsDuplicate(nums){
+    nums.sort((a,b)=> a - b)
+    
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === nums[i + 1]) return true
+        else return false
+    }
+}
 
+console.log(containsDuplicate([1,2,3,4]))
 
