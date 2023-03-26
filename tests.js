77d00922164d66515b67d4e527606859a267e5bb
DiegoCascavita <sportscenter857@gmail.com>
2023-03-26 14:08:01 -0500

    minimum difference

--- tests.js
+++ tests.js
@@ -1,11 +1,7 @@
-function replaceElements(arr) {
-    let max = -1;
-    for (let i = arr.length - 1; i >= 0; i--) {
-      let temp = arr[i];
-      arr[i] = max;
-      max = Math.max(max, temp);
-    }
-    return arr;
-  }
-console.log(replaceElements([17,18,5,4,6,1]))
+const minimumDifference = function(nums, k) {
+  if(nums.length === 1) return 0 
+
+  return nums.length - k
+}
+console.log(minimumDifference([90],1))
 
