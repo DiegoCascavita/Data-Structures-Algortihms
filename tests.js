2336ddc7b3f8bf0501be99da96d90b6ad4794ac9
DiegoCascavita <sportscenter857@gmail.com>
2023-04-03 08:47:00 -0500

    sliding window

--- tests.js
+++ tests.js
@@ -1,22 +1,19 @@
-/*In this example, we have an array of numbers arr and a window size k. The goal is to find the maximum sum of any subarray of k elements in the array.
+/*
+make a sliding window up to k
+iterate over the array
+if [i] === [j] return true
+else false
 */
+function containsNearbyDuplicate(nums, k) {
 
-function slidingWindow(arr, k) {
-   let maxSum = 0
-   // Loop through the array until left pointer have enough space "k"
-   for (let i = 0; i <= arr.length - k; i++){ 
-     let windowSum = 0; // Initialize a variable to store the window sum
- 
-     for (let j = i; j < i + k; j++) { // Loop "j" since "i" up to "k"
-       windowSum += arr[j]; // Add the current element to the window sum
-     }
-     maxSum = Math.max(maxSum, windowSum); // Update the maximum sum
-   }
- 
-   return maxSum;
- }
- 
- // Example usage:
- const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
- const k = 3;
- console.log(slidingWindow(arr, k)); // Output: 27
+  for(let i = 0; i < nums.length; i++){
+      // only check up to k
+      for(let j = i + 1; j <= i + k && j < nums.length; j++){
+          if(nums[i] === nums[j]) return true
+      }
+  }   
+  return false
+}
+console.log(containsNearbyDuplicate([99,99],3))
+// console.log(containsNearbyDuplicate([1,2,3,1],3))
+// console.log(containsNearbyDuplicate([1,2,3,1,2,3],2))
