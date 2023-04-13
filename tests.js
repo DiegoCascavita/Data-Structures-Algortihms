744903e52aaf4a4abec4b99a893e2ce87e707d86
DiegoCascavita <sportscenter857@gmail.com>
2023-04-13 15:10:21 -0500

    test

--- tests.js
+++ tests.js
@@ -1,25 +1,14 @@
-/**
-create a counter
-create a sliding window of size k
-create a temp to sum all the indices in the window
-sum / k = average
-if average >= threshold counter ++
-return counter
- */
-const numOfSubarrays = (arr, k, threshold) => {
-  let counter = 0;
-  
-  for (let i = 0; i <= arr.length - k; i++) {
-    let temp = 0;  
-    for (let j = i; j < i + k; j++) {
-      temp += arr[j];
-    } 
-    let average = temp / k;
-    
-    if (average >= threshold) {
-      counter++;
+function isPalindrome(str){
+  let left = 0
+  let right = str.length -1
+  while( left < right){
+    if(str[left] !== str[right]){
+      return false
     }
+    left ++
+    right --
   }
-  return counter;
-};
-console.log(numOfSubarrays([2,2,2,2,5,5,5,8],3,4))
+  return true
+}
+
+console.log(validPalindrome('abc'))
