2d67eed177e3b029debfc5abbc28579ec791501c
DiegoCascavita <sportscenter857@gmail.com>
2023-05-02 15:56:20 -0500

    remove duplicates

--- tests.js
+++ tests.js
@@ -1,22 +1,17 @@
 /**
-CREATE A MIN VAR
-CREATE A MAX VAR
-CREATE A SUM  VAR
+keep track of the var 
+if nums[i] === nums[i] + 1 erase
+only keep unique numbers 
+return k, arr
  */
-const average = function(salary) {
-    let sum  = 0
-    let min = salary[0]
-    let max = salary[0]
+var removeDuplicates = function(nums) {
+    let k = 0
 
-    //calculate max, min
-    for(let i = 0; i < salary.length; i++){
-        sum += salary[i]
-        if(salary[i] < min){
-            min = salary[i]
-        }
-        if( salary[i] > max){
-            max = salary[i]
+    for(let i = 0; i < nums.length; i ++){
+        if(nums[i] !== nums[k]){
+            k++
+            nums[k] = nums[i]
         }
     }
-    return (sum - min - max) / (salary.length - 2)
+    return k + 1
 };
