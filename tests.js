8dcd83916e58e53fa4667b0504c71e750621726b
DiegoCascavita <sportscenter857@gmail.com>
2023-05-09 09:52:49 -0500

    bublesort

--- tests.js
+++ tests.js
@@ -1,25 +1,22 @@
-function isValid(s) {
-    const stack = [];
-    const brackets = {
-      ')': '(',
-      '}': '{',
-      ']': '['
-    };
+function bubbleSort(arr) {
+    const n = arr.length;
   
-    for (let i = 0; i < s.length; i++) {
-      const char = s[i];
-      if (brackets[char]) {
-        // Closing bracket
-        const lastOpenBracket = stack.pop();
-        if (brackets[char] !== lastOpenBracket) {
-          return false;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          // intercambiar arr[j] y arr[j+1]
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
         }
-      } else {
-        // Opening bracket
-        stack.push(char);
       }
     }
-  
-    return stack.length === 0;
+    
+    return arr;
   }
+  
+  // Ejemplo de uso
+  const array = [64, 34, 25, 12, 22, 11, 90];
+  const sortedArray = bubbleSort(array);
+  console.log(sortedArray); // [11, 12, 22, 25, 34, 64, 90]
   
