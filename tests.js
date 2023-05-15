cbc50aa07fc0b2b179c04681083be532d8edd15a
DiegoCascavita <sportscenter857@gmail.com>
2023-05-15 13:00:55 -0500

    product

--- tests.js
+++ tests.js
@@ -1,22 +1,13 @@
-function bubbleSort(arr) {
-    const n = arr.length;
-  
-    for (let i = 0; i < n - 1; i++) {
-      for (let j = 0; j < n - i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-          // intercambiar arr[j] y arr[j+1]
-          const temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
-        }
-      }
-    }
-    
-    return arr;
+function product(a, b){
+  var result = 0;
+  if( b == 0 ) return result;
+ 
+  for(var x = 0; x < b; x++){
+    result += a;
   }
-  
-  // Ejemplo de uso
-  const array = [64, 34, 25, 12, 22, 11, 90];
-  const sortedArray = bubbleSort(array);
-  console.log(sortedArray); // [11, 12, 22, 25, 34, 64, 90]
-  
+ 
+  return result;
+}
+ 
+console.log( product( 3, 5 ) ); // 15
+console.log( product( 7, 0 ) ); // 0
