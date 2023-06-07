06d78ae7d51addc98456ecfb8c5c5538a9dfdbed
DiegoCascavita <sportscenter857@gmail.com>
2023-06-07 11:29:14 -0500

    matrix

--- tests.js
+++ tests.js
@@ -1,19 +1,21 @@
-function solution(sequence) {
-  let count = 0; // Number of violations
+function solution(matrix) {
+  let totalSum = 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
 
-  for (let i = 1; i < sequence.length; i++) {
-    if (sequence[i] <= sequence[i - 1]) {
-      count++;
-      if (count > 1) {
-        return false;
+  for (let j = 0; j < cols; j++) {
+    let isHaunted = false;
+
+    for (let i = 0; i < rows; i++) {
+      if (matrix[i][j] === 0) {
+        isHaunted = true;
       }
 
-      // Check if removing the current or previous element would fix the violation
-      if (i > 1 && i < sequence.length - 1 && sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
-        return false;
+      if (!isHaunted) {
+        totalSum += matrix[i][j];
       }
     }
   }
 
-  return true;
+  return totalSum;
 }
