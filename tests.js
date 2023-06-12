9e4b949b8902df3bd6b6c5fa7ff893ed05cd227f
DiegoCascavita <sportscenter857@gmail.com>
2023-06-12 09:18:09 -0500

    frequency counter

--- tests.js
+++ tests.js
@@ -1,21 +1,21 @@
-function solution(matrix) {
-  let totalSum = 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-
-  for (let j = 0; j < cols; j++) {
-    let isHaunted = false;
-
-    for (let i = 0; i < rows; i++) {
-      if (matrix[i][j] === 0) {
-        isHaunted = true;
-      }
-
-      if (!isHaunted) {
-        totalSum += matrix[i][j];
-      }
+/*
+1. sort the 2 arrays
+2. iterate trough both
+3. send the matches to a new array
+ */
+function solution(s1, s2) {
+    const s1Sorted = s1.sort((a,b)=> a - b)
+    const s2Sorted = s2.sort((a,b)=> a - b)
+    let result = []
+    
+    for(let i = 0; i < s1Sorted.length || i < s2Sorted.length; i++){
+        if(s1Sorted[i] === s2Sorted[i]){
+            result.push([i])
+        }
     }
-  }
-
-  return totalSum;
+    return result
 }
+
+let s1 = "aabcc"
+let s2 = "adcaa"
+console.log(solution)
